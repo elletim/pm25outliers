95b56451b9d75a8d455c303c9d7795d001b68c1a
elletim <113080146+elletim@users.noreply.github.com>
2022-10-27 11:35:38 -0700

    adjust legend

--- graph/views.py
+++ graph/views.py
@@ -40,15 +40,16 @@ def results(request):
     minusstdv1 = [average - std] * len(pm25_nums)
     plusstdv2 = [average + (2*std)] * len(pm25_nums)
     minusstdv2 = [average - (2*std)] * len(pm25_nums)
-    templabel = ['a'] 
-    templabel = templabel 
+    templabel = [1] 
+    templabel = templabel * len(pm25_list)
     def get_time(cityid):
         cursor.execute('''SELECT datetime FROM aq_data WHERE city_id = (%s)''', (cityid,))
         records = cursor.fetchall()
         return(records)
     datetime_list = get_time(cityid)
     datetime_nums = [x[0] for x in datetime_list]
+
     datetime_nums = [date_obj.strftime('%Y%m%d') for date_obj in datetime_nums]
     context = {'pm25_list' : pm25_nums, 'datetime_list': templabel, 'average': averagevar, 'std': std,
-    'plusstdv1' : plusstdv1, 'minusstdv1' : minusstdv1, 'plusstdv2' : plusstdv2, 'minusstdv2' : minusstdv2}
+    'plusstdv1' : plusstdv1, 'minusstdv1' : minusstdv1, 'plusstdv2' : plusstdv2, 'minusstdv2' : minusstdv2, 'city': selected_choice}
     return render(request, 'graph/results.html', context)
