84ff6ee10de251b8b8fa3d8a83fdfd8d9691b0bc
elletim <113080146+elletim@users.noreply.github.com>
2022-11-21 22:11:16 -0800

    plot all cities data

--- graph/views.py
+++ graph/views.py
@@ -30,20 +30,20 @@ def question3(request):
     return render(request, 'graph/question3.html', context)
 
 def get_id(selected_choice):
-        login = config.postgres["login"]
-        connection = psycopg2.connect(login)
-        cursor = connection.cursor()
-        cursor.execute('''SELECT city_id FROM aq_meta WHERE city = (%s)''', (selected_choice,))
-        records = cursor.fetchall()
-        return records
+    login = config.postgres["login"]
+    connection = psycopg2.connect(login)
+    cursor = connection.cursor()
+    cursor.execute('''SELECT city_id FROM aq_meta WHERE city = (%s)''', (selected_choice,))
+    records = cursor.fetchall()
+    return(records)
 
 def get_pm25(cityid):
-        login = config.postgres["login"]
-        connection = psycopg2.connect(login)
-        cursor = connection.cursor()
-        cursor.execute('''SELECT pm2_5, datetime FROM aq_data WHERE city_id = (%s) order by datetime''', (cityid,))
-        records = cursor.fetchall()
-        return records 
+    login = config.postgres["login"]
+    connection = psycopg2.connect(login)
+    cursor = connection.cursor()
+    cursor.execute('''SELECT pm2_5, datetime FROM aq_data WHERE city_id = (%s) order by datetime''', (cityid,))
+    records = cursor.fetchall()
+    return(records) 
 
 def get_df(cityid):
     login = config.postgres["login"]
@@ -58,6 +58,11 @@ def get_df(cityid):
     df1 = df1.groupby([df1['date'].dt.year, df1['date'].dt.month]).agg({'pm25':'mean'})
     return(df1)
 
+def get_fill(pm25, pm):
+    diff = len(pm25)-len(pm)
+    fill = [None] * diff
+    pm = fill + pm
+    return(pm)
 
 def results(request):
     question = Question.objects.get(pk=1)
@@ -79,7 +84,6 @@ def results(request):
     minpm25 = min(pm25_nums)
     maxpm25 = max(pm25_nums)
     datetime_nums = [date_obj.strftime('%Y/%m/%d/%H') for date_obj in datetime_nums]
-    #datetime_nums = [int(x) for x in datetime_nums]
     startvalue = datetime_nums[0]
     merge = [list(i) for i in zip(datetime_nums, pm25_nums)]
     context = {'pm25_list' : pm25_nums, 'datetime_list': json.dumps(datetime_nums), 'average': averagevar, 'std': std,
@@ -114,9 +118,7 @@ def results2(request):
     stdv_1 = [average - (std)] * len(pm25_nums)
     minpm25 = min(pm25_nums)
     maxpm25 = max(pm25_nums)
-   
     startvalue = datetime_nums[0]
-    merge = [list(i) for i in zip(datetime_nums, pm25_nums)]
     
     #graph 2
     login = config.postgres["login"]
@@ -124,23 +126,62 @@ def results2(request):
     cursor = connection.cursor()
     cursor.execute('''SELECT city FROM aq_meta''')
     citynames = cursor.fetchall()
-    df1 = get_df(0)
-    df2 = get_df(3)
-    pm25 = df1['pm25'].to_list()
+    citynames = [i[0] for i in citynames]
+    print(citynames[1])
+    df = get_df(0)
+    pm25 = df['pm25'].to_list()
+    df1= get_df(1)
+    pm25_1 = df1['pm25'].to_list()
+    pm25_1 = get_fill(pm25, pm25_1)
+    df2= get_df(2)
     pm25_2 = df2['pm25'].to_list()
-    diff = len(pm25)-len(pm25_2)
-    fill = [0] * diff
-    pm25_2 = fill + pm25_2
-    x = list(df1.index.values)
+    pm25_2 = get_fill(pm25, pm25_2)
+    df3= get_df(3)
+    pm25_3 = df3['pm25'].to_list()
+    pm25_3 = get_fill(pm25, pm25_3)
+    df4= get_df(4)
+    pm25_4 = df4['pm25'].to_list()
+    pm25_4 = get_fill(pm25, pm25_4)
+    df5= get_df(5)
+    pm25_5 = df5['pm25'].to_list()
+    pm25_5 = get_fill(pm25, pm25_5)
+    df6= get_df(1)
+    pm25_6 = df6['pm25'].to_list()
+    pm25_6 = get_fill(pm25, pm25_6)
+    df7= get_df(7)
+    pm25_7 = df7['pm25'].to_list()
+    pm25_7 = get_fill(pm25, pm25_7)
+    df8= get_df(8)
+    pm25_8 = df8['pm25'].to_list()
+    pm25_8 = get_fill(pm25, pm25_8)
+    df9= get_df(9)
+    pm25_9 = df9['pm25'].to_list()
+    pm25_9 = get_fill(pm25, pm25_9)
+    df10= get_df(10)
+    pm25_10 = df10['pm25'].to_list()
+    pm25_10 = get_fill(pm25, pm25_10)
+    df11= get_df(11)
+    pm25_11 = df11['pm25'].to_list()
+    pm25_11 = get_fill(pm25, pm25_11)
+    df12= get_df(12)
+    pm25_12 = df12['pm25'].to_list()
+    pm25_12 = get_fill(pm25, pm25_12)
+    df13= get_df(13)
+    pm25_13 = df13['pm25'].to_list()
+    pm25_13 = get_fill(pm25, pm25_13)
+    x = list(df.index.values)
     year1 = [i[0] for i in x]
     month1 = [i[1] for i in x]
     datetime= [str(i) for i in zip(year1, month1)]
-    datetime = [i.replace(',','').replace(' ','').replace('(','').replace(')','') for i in datetime]
-    datetime= [int(x) for x in datetime]
+    datetime = [i.replace(',','/').replace(' ','').replace('(','').replace(')','') for i in datetime]
 
     context = {'pm25_list' : pm25_nums, 'datetime_list': json.dumps(datetime_nums), 'average': averagevar, 'std': std,
-    'plusstdv1' : stdv1, 'minusstdv1' : stdv_1, 'pm25' : pm25, 'pm25_2': pm25_2, 'datetime' : datetime,
-    'city': selected_choice, 'minpm25': minpm25, 'maxpm25': maxpm25, 'merge': merge, 'startvalue': startvalue}
+    'plusstdv1' : stdv1, 'minusstdv1' : stdv_1, 'pm25' : pm25, 
+    'pm25_1': json.dumps(pm25_1), 'pm25_2': json.dumps(pm25_2), 'pm25_3': json.dumps(pm25_3), 'pm25_4': json.dumps(pm25_4), 
+    'pm25_5': json.dumps(pm25_5), 'pm25_6': json.dumps(pm25_6), 'pm25_7': json.dumps(pm25_7), 'pm25_8': json.dumps(pm25_8),
+    'pm25_9': json.dumps(pm25_9), 'pm25_10': json.dumps(pm25_10), 'pm25_11': json.dumps(pm25_11), 'pm25_12': json.dumps(pm25_12),
+    'pm25_12': json.dumps(pm25_12), 'pm25_13': json.dumps(pm25_13), 'datetime' : json.dumps(datetime), 'citynames': json.dumps(citynames),
+    'city': selected_choice, 'minpm25': minpm25, 'maxpm25': maxpm25, 'startvalue': startvalue}
     return render(request, 'graph/results2.html', context)
 
 def results3(request):
